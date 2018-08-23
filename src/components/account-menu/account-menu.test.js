342035d1bc999e1e8cde44fe1790b0a997739d0a
Gordon McMullan <gordon.mcmullan@digital.hmrc.gov.uk>
2018-08-23 16:58:36 +0100

    second set of tests for the desktop menu working

--- src/components/account-menu/account-menu.test.js
+++ src/components/account-menu/account-menu.test.js
@@ -17,7 +17,8 @@ beforeAll(async (done) => {
   page = await browser.newPage()
   await page.goto(baseUrl + '/components/account-menu/default/preview')
   const nav = await page.evaluate(() => document.getElementById('secondary-nav'))
-  console.log({...nav})
+  console.log({ ...nav
+  })
   done()
 })
 
@@ -33,18 +34,25 @@ describe('/components/account-menu', () => {
     it('should display with a closed subnav on the page when loaded', async (done) => {
       // ToDo: wrap all this into a single awaitable function
       /* ToDo: work out how to passs in the id as a variable
-               subNavId = await page.evaluate(() => {
-                 document.getElementById('account-menu__main-2').getAttribute('aria-owns'))
+            subNavId = await page.evaluate(() => {
+                document.getElementById('account-menu__main-2')
+                        .getAttribute('aria-owns'))
                 } */
 
       const classList = await page.evaluate(() => {
-        return document.getElementById('subnav-your-account').classList
+        return document
+          .getElementById('subnav-your-account')
+          .className.split(' ')
       })
       const ariaHidden = await page.evaluate(() => {
-        return document.getElementById('subnav-your-account').getAttribute('aria-hidden')
+        return document
+          .getElementById('subnav-your-account')
+          .getAttribute('aria-hidden')
       })
       const ariaExpanded = await page.evaluate(() => {
-        return document.getElementById('account-menu__main-2').getAttribute('aria-expanded')
+        return document
+          .getElementById('account-menu__main-2')
+          .getAttribute('aria-expanded')
       })
 
       expect(classList).not.toContain('hmrc-subnav-reveal')
@@ -53,11 +61,46 @@ describe('/components/account-menu', () => {
       done()
     })
 
-    it('Should have all mobile elements hidden', () => {
-      return Promise.reject(new Error('not implemented'))
+    it('Should have all mobile elements hidden', async (done) => {
+      const classList = await page.evaluate(() => {
+        return document
+          .getElementsByClassName('hmrc-account-menu__link--menu')[0]
+          .className.split(' ')
+      })
+      const ariaHidden = await page.evaluate(() => {
+        return document
+          .getElementsByClassName('hmrc-account-menu__link--menu')[0]
+          .getAttribute('aria-hidden')
+      })
+      const ariaExpanded = await page.evaluate(() => {
+        return document
+          .getElementsByClassName('hmrc-account-menu__link--menu')[0]
+          .getAttribute('aria-expanded')
+      })
+
+      expect(classList).toContain('js-hidden')
+      expect(ariaHidden).toBe('true')
+      expect(ariaExpanded).toBe('false')
+      done()
+    })
+
+    it('Should have the mobile \'back\' menu item hidden', async (done) => {
+      const classList = await page.evaluate(() => {
+        return document
+          .getElementsByClassName('hmrc-account-menu__link--back')[0]
+          .className.split(' ')
+      })
+      const ariaHidden = await page.evaluate(() => {
+        return document
+          .getElementsByClassName('hmrc-account-menu__link--back')[0]
+          .getAttribute('aria-hidden')
+      })
+
+      expect(classList).toContain('hidden')
+      expect(ariaHidden).toBe('true')
+      done()
     })
   })
-
   // Opening the account menu
   describe('When \'Your account\' is opened', () => {
     it('should reveal the subnav', () => {
