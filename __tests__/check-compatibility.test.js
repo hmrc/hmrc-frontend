d88b8084c53373fd27d204636f6499ab836fc76c
Silas Landricombe <1029540+silasl@users.noreply.github.com>
2019-12-04 11:20:49 +0000

    PLATUI-288: Comprehensive versioning (#56)
    ![](https://media.giphy.com/media/l1ug4IxJN7B7A6PpS/giphy.gif)
    
    * Added unit tests for version compatibility checking
    * Assume support for future versions of the prototype kit
    * Add support for versions that are compatible with manual steps

---
+++ __tests__/check-compatibility.test.js
@@ -0,0 +1,185 @@
+/* eslint-env jest */
+import { writeFileSync } from 'fs'
+import path from 'path'
+import { exec } from 'child_process'
+
+describe('Version compatibility check', () => {
+  expect.extend({
+    toContainPartial (array, str) {
+      const pass = array.some(x => x.includes(str))
+      return {
+        message: () => `expected ${array}${pass ? ' NOT' : ''} to contain an item with ${str}`,
+        pass
+      }
+    }
+  })
+
+  const scriptPath = path.resolve(__dirname, '../check-compatibility.js')
+  const mockPackagePath = path.resolve(__dirname, 'package.json')
+  const mockEnv = { env: { ...process.env, 'INIT_CWD': './__tests__' } }
+
+  const createMockPackage = (contents) => writeFileSync(mockPackagePath, JSON.stringify(contents))
+
+  let logs
+  let errors
+
+  beforeEach(() => {
+    logs = []
+    errors = []
+  })
+
+  describe('Installing outside of Prototype kit', () => {
+    it('should exit the process with code 0', (done) => {
+      createMockPackage({
+        name: 'foo'
+      })
+
+      const child = exec(`node ${scriptPath}`, mockEnv)
+      child.stdout.on('data', (data) => logs.push(data))
+      child.stderr.on('data', (error) => errors.push(error))
+      child.on('error', (error) => errors.push(error))
+
+      child.on('exit', (code) => {
+        expect(code).toBe(0)
+        expect(errors.length).toBe(0)
+        expect(logs.length).toBe(0)
+        done()
+      })
+    })
+  })
+
+  describe('Installing inside of Prototype kit', () => {
+    describe('Installing a compatible version', () => {
+      it('should exit the process with code 0', (done) => {
+        createMockPackage({
+          name: 'govuk-prototype-kit',
+          version: '9.4.0'
+        })
+
+        const child = exec(`node ${scriptPath}`, mockEnv)
+        child.stdout.on('data', (data) => logs.push(data))
+        child.stderr.on('data', (error) => errors.push(error))
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', (code) => {
+          expect(code).toBe(0)
+          expect(errors.length).toBe(0)
+          expect(logs.length).toBe(0)
+          done()
+        })
+      })
+    })
+
+    describe('Installing a future version', () => {
+      it('should exit the process with code 0', (done) => {
+        createMockPackage({
+          name: 'govuk-prototype-kit',
+          version: '100.0.0'
+        })
+
+        const child = exec(`node ${scriptPath}`, mockEnv)
+        child.stdout.on('data', (data) => { console.log('data: ', data); logs.push(data) })
+        child.stderr.on('data', (error) => { console.log('error: ', error); errors.push(error) })
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', (code) => {
+          expect(code).toBe(0)
+          expect(errors.length).toBe(0)
+          expect(logs.length).toBe(0)
+          done()
+        })
+      })
+    })
+
+    describe('Installing a non-compatible version, but a compatible version exists', () => {
+      it('should exit the process with code 1 and provide instructions for installing a compatible version if no other steps are required', (done) => {
+        createMockPackage({
+          name: 'govuk-prototype-kit',
+          version: '8.7.2'
+        })
+
+        const child = exec(`node ${scriptPath}`, mockEnv)
+        child.stdout.on('data', (data) => logs.push(data))
+        child.stderr.on('data', (error) => errors.push(error))
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', (code) => {
+          expect(code).toBe(1)
+          expect(errors.length).toBe(0)
+          expect(logs).toContainPartial('npm install hmrc-frontend@0.6.x')
+          done()
+        })
+      })
+
+      it('should offer information on installing a compatible version as well as instructions on manual steps required', (done) => {
+        createMockPackage({
+          name: 'express-prototype',
+          version: '7.1.0'
+        })
+
+        const child = exec(`node ${scriptPath}`, { env: { ...mockEnv.env, atPrompt: 'y' } })
+        child.stdout.on('data', (data) => logs.push(data))
+        child.stderr.on('data', (error) => errors.push(error))
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', (code) => {
+          expect(code).toBe(1)
+          expect(errors.length).toBe(0)
+          expect(logs).toContainPartial('https://design.tax.service.gov.uk/hmrc-design-patterns/install-hmrc-frontend-in-your-prototype/install-hrmc-frontend-in-an-old-version-of-the-govuk-prototype-kit/')
+          done()
+        })
+      })
+    })
+
+    describe('Installing a non-compatible version, and a compatible version does not exist', () => {
+      beforeEach(() => {
+        createMockPackage({
+          name: 'express-prototype',
+          version: '6.0.0'
+        })
+      })
+
+      it('should offer information on upgrading the prototype kit', (done) => {
+        const child = exec(`node ${scriptPath}`, { env: { ...mockEnv.env, atPrompt: 'y' } })
+        child.stdout.on('data', (data) => logs.push(data))
+        child.stderr.on('data', (error) => errors.push(error))
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', () => {
+          expect(errors.length).toBe(0)
+          expect(logs).toContainPartial('Your prototype is not compatible with HMRC Frontend')
+          expect(logs).toContainPartial('https://govuk-prototype-kit.herokuapp.com/docs/updating-the-kit')
+          done()
+        })
+      })
+
+      it('should exit the process with code 0 when user chooses to continue', (done) => {
+        const child = exec(`node ${scriptPath}`, { env: { ...mockEnv.env, atPrompt: 'y' } })
+        child.stdout.on('data', (data) => logs.push(data))
+        child.stderr.on('data', (error) => errors.push(error))
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', (code) => {
+          expect(code).toBe(0)
+          done()
+        })
+      })
+
+      it('should exit the process with code 1 when user chooses NOT to continue', (done) => {
+        const child = exec(`node ${scriptPath}`, { env: { ...mockEnv.env, atPrompt: 'n' } })
+        child.stdout.on('data', (data) => logs.push(data))
+        child.stderr.on('data', (error) => errors.push(error))
+        child.on('error', (error) => errors.push(error))
+
+        child.on('exit', (code) => {
+          expect(code).toBe(1)
+          done()
+        })
+      })
+    })
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+})
--- check-compatibility.js
+++ check-compatibility.js
@@ -1,7 +1,8 @@
-
 const consumerRoot = process.env.INIT_CWD
 const consumerPackageJson = require(`${consumerRoot}/package.json`)
 const hmrcFrontendPackageJson = require('./package.json')
+
+const withManualSteps = 'withManualSteps'
 
 const knownPrototypeKitNames = ['govuk-prototype-kit', 'express-prototype']
 
@@ -11,11 +12,14 @@ if (!knownPrototypeKitNames.includes(consumerPackageJson.name)) {
 }
 
 const compatibility = {
-  '1.0': {
-    'prototype-kit': ['9.2', '9.1', '9.0']
+  '1.6': {
+    'prototype-kit': ['9.4', '9.3', '9.2', '9.1', '9.0']
   },
   '0.6': {
     'prototype-kit': ['8.12', '8.11', '8.10', '8.9', '8.8', '8.7']
+  },
+  [`0.6${withManualSteps}`]: {
+    'prototype-kit': ['8.6', '8.5', '8.4', '8.3', '8.2', '8.1', '8.0', '7.1', '7.0']
   }
 }
 
@@ -32,27 +36,55 @@ const reset = '\x1b[0m'
 
 const hmrcFrontendVersion = hmrcFrontendPackageJson.version
 const compatibilityVersion = Object.keys(compatibility)
-  .filter(version => parseFloat(version) < parseFloat(hmrcFrontendVersion))[0]
+  .find(version => parseFloat(version) < parseFloat(hmrcFrontendVersion))
 
 const checkCompatibility = (dependency, version) => {
-  const versionMatcher = String(parseFloat(version).toFixed(1))
-  const compatible = !!compatibility[compatibilityVersion][dependency].find(v => String(parseFloat(v)) === versionMatcher)
-  return { version, compatible, versionMatcher }
+  const getMatchableVersion = (v) => String(parseFloat(v).toFixed(1))
+  const versionMatcher = getMatchableVersion(version)
+  const compatibleVersions = compatibility[compatibilityVersion][dependency]
+
+  let compatible =
+    // Version is newer than our compatibility matrix
+    parseFloat(compatibleVersions[0]) < parseFloat(versionMatcher) ||
+    // Version is compatible
+    !!compatibleVersions.find(v => getMatchableVersion(v) === versionMatcher)
+
+  let alternativeVersion = Object.keys(compatibility)
+    .find(version => compatibility[version]['prototype-kit'].includes(versionMatcher))
+
+  let requiresManualSteps = false
+  if (alternativeVersion && alternativeVersion.includes(withManualSteps)) {
+    alternativeVersion = alternativeVersion.replace(withManualSteps, '')
+    requiresManualSteps = true
+    compatible = true
+  }
+
+  return { version, compatible, alternativeVersion, requiresManualSteps }
 }
 
 const styleString = (str, colour = green, style = '') => `${colour}${style}${str}${reset}`
 
-const prototypeKitVersion = checkCompatibility('prototype-kit', consumerPackageJson.version)
+const { alternativeVersion, compatible, requiresManualSteps } = checkCompatibility('prototype-kit', consumerPackageJson.version)
 
-if (prototypeKitVersion.compatible) {
+if (compatible && !requiresManualSteps) {
   process.exit(0)
 }
 
-if (!prototypeKitVersion.compatible) {
-  const alternativeVersion = Object.keys(compatibility)
-    .filter(version => compatibility[version]['prototype-kit'].includes(prototypeKitVersion.versionMatcher))
+if (!compatible || requiresManualSteps) {
+  if (alternativeVersion && requiresManualSteps) {
+    console.log(
+      styleString('The version of HMRC Frontend you are trying to install is not compatible with your version of the GOV.UK Prototype Kit.'),
+      '\n\n'
+    )
 
-  if (alternativeVersion.length) {
+    console.log('You can install a compatible version by following the steps detailed below')
+    console.log(
+      styleString('https://design.tax.service.gov.uk/hmrc-design-patterns/install-hmrc-frontend-in-your-prototype/install-hrmc-frontend-in-an-old-version-of-the-govuk-prototype-kit/', blue, underline),
+      '\n\n'
+    )
+
+    process.exit(1)
+  } else if (alternativeVersion) {
     console.log(
       styleString('The version of HMRC Frontend you are trying to install is not compatible with your version of the GOV.UK Prototype Kit.'),
       '\n\n'
@@ -60,21 +92,25 @@ if (!prototypeKitVersion.compatible) {
     console.log('You can install a compatible version by typing', '\n\n')
 
     console.log('┌─────────────────────────────────┐')
-    console.log(`| npm install hmrc-frontend@${alternativeVersion[0]}.x |`)
+    console.log(`| npm install hmrc-frontend@${alternativeVersion}.x |`)
     console.log('└─────────────────────────────────┘', '\n\n')
+
     process.exit(1)
   } else {
     console.log(styleString('Your prototype is not compatible with HMRC Frontend'), '\n\n')
     console.log('You are using an old version of the GOV.UK Prototype Kit. This means it is not compatible with HMRC Frontend. You need to update to the latest version of the GOV.UK Prototype Kit.', '\n\n')
     console.log('Find out more about updating your GOV.UK prototype')
     console.log(styleString('https://govuk-prototype-kit.herokuapp.com/docs/updating-the-kit', blue, underline), '\n\n')
-    readline.question('You can continue to install HMRC Frontend, but your prototype may look different or some features may not work. Do you want to continue? (y/n) ', (answer) => {
-      console.log('\n\n')
-      if (answer[0].toLowerCase() === 'y') {
-        process.exit(0)
-      } else {
-        process.exit(1)
-      }
-    })
+
+    const acceptPrompt = (response) => process.exit((response === 'y') ? 0 : 1)
+
+    if (process.env.atPrompt) {
+      acceptPrompt(process.env.atPrompt)
+    } else {
+      readline.question('You can continue to install HMRC Frontend, but your prototype may look different or some features may not work. Do you want to continue? (y/n) ', (answer) => {
+        console.log('\n\n')
+        acceptPrompt(answer[0].toLowerCase())
+      })
+    }
   }
 }
