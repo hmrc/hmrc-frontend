dde1803365a7b10a774431b01f8638e9d1b4d6c0
Vincent Vinnicombe <->
2020-03-30 11:40:29 +0100

    relative-paths: Removed node_modules from pipe sass includePaths

--- tasks/gulp/compile-assets.js
+++ tasks/gulp/compile-assets.js
@@ -52,9 +52,6 @@ gulp.task('scss:compile', () => {
   let compile = gulp.src(compileStylesheet)
     .pipe(plumber(errorHandler))
     .pipe(gulpif(!isPackage, sourcemaps.init()))
-    .pipe(sass({
-      includePaths: ['node_modules']
-    }))
     // minify css add vendor prefixes and normalize to compiled css
     .pipe(gulpif(isDist, postcss([
       autoprefixer,
