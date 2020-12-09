1b42c1479f5b9211938c430ce68a25ccce77a319
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-12-09 14:47:30 +0000

    PLATUI-792: Replace serviceCode with serviceId for consistency with contact-frontend

--- src/components/report-technical-issue/template.test.js
+++ src/components/report-technical-issue/template.test.js
@@ -27,7 +27,7 @@ describe('Report Technical Issue', () => {
       expect($component.attr('target')).toEqual('_blank')
       expect($component.attr('hreflang')).toEqual('en')
       expect($component.attr('lang')).toEqual('en')
-      expect($component.attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=the-url-safe-service-name')
+      expect($component.attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=the-url-safe-service-id')
       expect($component.text()).toEqual('Is this page not working properly? (opens in new tab)')
     })
   })
@@ -41,6 +41,18 @@ describe('Report Technical Issue', () => {
     expect($component.attr('hreflang')).toEqual('en')
     expect($component.attr('lang')).toEqual('en')
     expect($component.attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true')
+    expect($component.text()).toEqual('Is this page not working properly? (opens in new tab)')
+  })
+
+  it('renders a link element when using the deprecated serviceCode parameter', () => {
+    const $ = render('report-technical-issue', examples['with-deprecated-service-code'])
+
+    const $component = $('.govuk-link')
+    expect($component.get(0).tagName).toEqual('a')
+    expect($component.attr('target')).toEqual('_blank')
+    expect($component.attr('hreflang')).toEqual('en')
+    expect($component.attr('lang')).toEqual('en')
+    expect($component.attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=the-url-safe-service-code')
     expect($component.text()).toEqual('Is this page not working properly? (opens in new tab)')
   })
 
@@ -87,20 +99,20 @@ describe('Report Technical Issue', () => {
     expect($('.govuk-link').attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=my-generic-service')
   })
 
-  it('should URL encode the service name', () => {
-    const $ = render('report-technical-issue', examples['with-non-url-safe-service-name'])
+  it('should URL encode the service identifier', () => {
+    const $ = render('report-technical-issue', examples['with-non-url-safe-service-id'])
 
     expect($('.govuk-link').attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=Build%20%26%20Deploy')
   })
 
-  it('should URL encode the service name', () => {
-    const $ = render('report-technical-issue', examples['with-redirect-url'])
+  it('should URL encode the referrer url', () => {
+    const $ = render('report-technical-issue', examples['with-referrer-url'])
 
     expect($('.govuk-link').attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=pay&referrerUrl=https%3A%2F%2Fwww.tax.service.gov.uk%2Fpay%3Fabc%3Ddef')
   })
 
-  it('should URL encode the service name', () => {
-    const $ = render('report-technical-issue', examples['with-local-redirect-url'])
+  it('should URL encode the local referrer url', () => {
+    const $ = render('report-technical-issue', examples['with-local-referrer-url'])
 
     expect($('.govuk-link').attr('href')).toEqual('/contact/problem_reports_nonjs?newTab=true&service=pay&referrerUrl=http%3A%2F%2Flocalhost%3A9123%2Fmy-service')
   })
