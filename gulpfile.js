eb4c6e1df84ef23693b4e0e6e2bfa6cac4358ee1
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2021-01-25 15:48:48 +0000

    PLATUI-934: Updated package task to include scss compilation and to pull in govuk-frontend

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-const gulp = require('gulp');
-const nodemon = require('nodemon');
-const fs = require('fs');
-const paths = require('./config/paths.json');
-const taskArguments = require('./tasks/gulp/task-arguments');
-const packageFile = require('./package.json');
+const gulp = require('gulp')
+const nodemon = require('nodemon')
+const fs = require('fs')
+const paths = require('./config/paths.json')
+const taskArguments = require('./tasks/gulp/task-arguments')
+const packageFile = require('./package.json')
 
 // Gulp sub-tasks
-require('./tasks/gulp/clean.js');
-require('./tasks/gulp/compile-assets.js');
-require('./tasks/gulp/watch.js');
+require('./tasks/gulp/clean.js')
+require('./tasks/gulp/compile-assets.js')
+require('./tasks/gulp/watch.js')
 // new tasks
-require('./tasks/gulp/copy-to-destination.js');
-require('./tasks/gulp/asset-version.js');
-require('./tasks/gulp/backstop.js');
+require('./tasks/gulp/copy-to-destination.js')
+require('./tasks/gulp/asset-version.js')
+require('./tasks/gulp/backstop.js')
 
 // Copy assets task ----------------------
 // Copies assets to taskArguments.destination (public)
 // --------------------------------------
 gulp.task('copy:assets', () => gulp
   .src(`${paths.src}assets/**/*`)
-  .pipe(gulp.dest(`${taskArguments.destination}/assets/`)));
+  .pipe(gulp.dest(`${taskArguments.destination}/assets/`)))
 
 gulp.task('copy:README', () => gulp
   .src(`${paths.src}../README.md`)
-  .pipe(gulp.dest(taskArguments.destination)));
+  .pipe(gulp.dest(taskArguments.destination)))
 
 gulp.task('copy:packageJson', (done) => {
   const requiredKeys = [
@@ -38,55 +38,55 @@ gulp.task('copy:packageJson', (done) => {
     'bugs',
     'homepage',
     'dependencies',
-    'scripts',
-  ];
+    'scripts'
+  ]
 
   Object.keys(packageFile).forEach((key) => {
     if (!requiredKeys.includes(key)) {
-      delete packageFile[key];
+      delete packageFile[key]
     }
-  });
+  })
 
   const requiredScripts = [
-    'preinstall',
-  ];
+    'preinstall'
+  ]
 
   Object.keys(packageFile.scripts).forEach((key) => {
     if (!requiredScripts.includes(key)) {
-      delete packageFile.scripts[key];
+      delete packageFile.scripts[key]
     }
-  });
+  })
 
   fs.writeFileSync(
     `${taskArguments.destination}/package.json`,
-    JSON.stringify(packageFile, null, 2),
-  );
+    JSON.stringify(packageFile, null, 2)
+  )
 
-  done();
-});
+  done()
+})
 
 // Compile scss and js assets
 // --------------------------------------
-gulp.task('compile-assets', gulp.series('scss:compile', 'js:compile'));
+gulp.task('compile-assets', gulp.series('scss:compile', 'js:compile'))
 
 // Compile and copy assets
 // --------------------------------------
-gulp.task('compile-and-copy-assets', gulp.series('compile-assets', 'copy-dist-files'));
+gulp.task('compile-and-copy-assets', gulp.series('compile-assets', 'copy-dist-files'))
 
 gulp.task('nodemon', () => nodemon({
-  script: 'app/start.js',
-}));
+  script: 'app/start.js'
+}))
 
 // Serve task ---------------------------
 // Restarts node app when there is changed
 // affecting js, css or njk files
 // --------------------------------------
-gulp.task('serve', gulp.parallel('nodemon', 'watch'));
+gulp.task('serve', gulp.parallel('nodemon', 'watch'))
 
 // Dev task -----------------------------
 // Runs a sequence of task on start
 // --------------------------------------
-gulp.task('dev', gulp.series('clean', 'compile-and-copy-assets', 'serve'));
+gulp.task('dev', gulp.series('clean', 'compile-and-copy-assets', 'serve'))
 
 // Build package task -----------------
 // Prepare package folder for publishing
@@ -97,15 +97,17 @@ gulp.task('build:package', gulp.series(
   'copy-govuk-config',
   'copy-check-compatibility',
   'js:compile',
+  'scss:compile',
+  'update-assets-version',
   'copy:README',
-  'copy:packageJson',
-));
+  'copy:packageJson'
+))
 
 gulp.task('build:dist', gulp.series(
   'clean',
   'compile-and-copy-assets',
   'copy:assets',
-  'update-assets-version',
-));
+  'update-assets-version'
+))
 
-gulp.task('backstop:test', gulp.series('compile-and-copy-assets', 'backstop-test'));
+gulp.task('backstop:test', gulp.series('compile-and-copy-assets', 'backstop-test'))
--- tasks/gulp/asset-version.js
+++ tasks/gulp/asset-version.js
@@ -1,32 +1,26 @@
-const fs = require('fs');
-const gulp = require('gulp');
-const del = require('del');
-const gulpif = require('gulp-if');
-const rename = require('gulp-rename');
-const replace = require('gulp-replace');
-const vinylPaths = require('vinyl-paths');
-const taskArguments = require('./task-arguments');
-const pkg = require('../../package.json');
-
-// check for the flag passed by the task
-
-const isDist = taskArguments.destination === 'dist' || false;
+const fs = require('fs')
+const gulp = require('gulp')
+const del = require('del')
+const rename = require('gulp-rename')
+const replace = require('gulp-replace')
+const vinylPaths = require('vinyl-paths')
+const taskArguments = require('./task-arguments')
+const pkg = require('../../package.json')
 
 // Update assets' versions ----------
 // Add all.package.json version
 // ----------------------------------
 gulp.task('update-assets-version', () => {
-  fs.writeFileSync(`${taskArguments.destination}/VERSION.txt`, `${pkg.version}\r\n`);
+  fs.writeFileSync(`${taskArguments.destination}/VERSION.txt`, `${pkg.version}\r\n`)
   return gulp.src([
-    `${taskArguments.destination}/**/*.min.*`,
+    `${taskArguments.destination}/**/*.min.*`
   ])
     .pipe(vinylPaths(del))
-    .pipe(gulpif(isDist,
-      rename((obj) => {
-        // eslint-disable-next-line no-param-reassign
-        obj.basename = obj.basename.replace(/(hmrc.*)(?=\.min)/g, `$1-${pkg.version}`);
-        return obj;
-      })))
+    .pipe(rename((obj) => {
+      // eslint-disable-next-line no-param-reassign
+      obj.basename = obj.basename.replace(/(hmrc.*)(?=\.min)/g, `$1-${pkg.version}`)
+      return obj
+    }))
     .pipe(replace(/(sourceMappingURL=maps\/hmrc.*)(?=\.min)/g, `$1-${pkg.version}`))
-    .pipe(gulp.dest(`${taskArguments.destination}/`));
-});
+    .pipe(gulp.dest(`${taskArguments.destination}/`))
+})
