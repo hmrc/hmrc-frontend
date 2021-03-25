dc020806eeffae36bc624ba612e854d412c88b7c
Tim Bryan <timsb@users.noreply.github.com>
2021-03-25 13:26:31 +0000

    updated check-compatibility

--- check-compatibility.js
+++ check-compatibility.js
@@ -14,7 +14,7 @@ if (!knownPrototypeKitNames.includes(consumerPackageJson.name)) {
 }
 
 const compatibility = {
-  1.29: {
+  1.30: {
     'prototype-kit': ['9.12', '9.11', '9.10', '9.9', '9.8', '9.7', '9.6', '9.5', '9.4', '9.3', '9.2', '9.1', '9.0', '9.9', '9.10', '9.11'],
   },
   0.6: {
