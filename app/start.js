e65f906b18f17a4309a8feed7c2805b1d928d2dc
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-01-11 11:54:09 +0000

    PLATUI-1532: Linting fixes

--- app/start.js
+++ app/start.js
@@ -2,7 +2,7 @@ const configPaths = require('../config/paths.json');
 
 const PORT = process.env.PORT || configPaths.ports.app;
 
-const app = require('./app.js')();
+const app = require('./app')();
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
