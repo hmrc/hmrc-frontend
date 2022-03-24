1f015d5729ad41bde7f8d40b5ee9e3ea95ece7f7
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2022-03-24 16:33:19 +0000

    PLATUI-1646: add extra VRT tests for account header (#216)
    Added a new helper for performing some steps before taking a VRT
    screenshot which was needed to capture some link states for the account
    menu before we fix some accessibility issues with their colour contrast

---
+++ backstop_data/engine_scripts/puppet/beforeTakingScreenshotHelper.js
@@ -0,0 +1,28 @@
+module.exports = async (page, { beforeTakingScreenshot }) => {
+  const allowedPageActions = {}
+
+  allowedPageActions.click = async selector =>
+    await page.click(selector);
+
+  allowedPageActions.hover = async selector =>
+    await page.hover(selector);
+
+  allowedPageActions.focus = async selector =>
+    await page.focus(selector);
+
+  allowedPageActions.type = async ({ into, text }) =>
+    await page.type(into, text);
+
+  allowedPageActions.press = async key =>
+    await page.press(key);
+
+  allowedPageActions.waitFor = async selector =>
+    await page.waitFor(selector);
+
+  if (beforeTakingScreenshot) {
+    for (const step of beforeTakingScreenshot) {
+      const [action, params] = Object.entries(step)[0];
+      await allowedPageActions[action](params);
+    }
+  }
+};
--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -1,6 +1,7 @@
 module.exports = async (page, scenario, vp) => {
   console.log(`SCENARIO > ${scenario.label}`);
   await require('./clickAndHoverHelper')(page, scenario);
+  await require('./beforeTakingScreenshotHelper')(page, scenario);
 
   // add more ready handlers here...
 };
