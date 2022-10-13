a8b7918883b00d4f42d76af1c980fc8b61d17ebe
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2022-10-13 10:14:49 +0100

    Example of how to customise search queries in the accessible autocomplete component

--- src/components/accessible-autocomplete/accessible-autocomplete.js
+++ src/components/accessible-autocomplete/accessible-autocomplete.js
@@ -6,6 +6,11 @@ function AccessibleAutoComplete($module, window, document) {
 
 AccessibleAutoComplete.prototype.init = function init() {
   if (this.$module) {
+    const trimQuery = (values) => (query, syncResults) => {
+      const matches = values.filter((r) => r.toLowerCase()
+        .indexOf(query.toLowerCase().trim()) !== -1);
+      syncResults(matches);
+    };
     const showAllValues = (this.$module.getAttribute('data-show-all-values') === 'true');
     const autoselect = (this.$module.getAttribute('data-auto-select') === 'true');
     const defaultValue = this.$module.getAttribute('data-default-value');
@@ -14,6 +19,7 @@ AccessibleAutoComplete.prototype.init = function init() {
       showAllValues,
       autoselect,
       defaultValue,
+      source: trimQuery(Array.from(this.$module.options).map((a) => a.innerText)),
     });
   }
 };
