73f423fa8fddecdd5e21f865ecb2e84c044ef88e
david.ellam <ellamdav@users.noreply.github.com>
2023-11-06 11:56:24 +0000

    PLATUI-2638 supply Welsh translations if language is set to 'cy'

--- src/components/accessible-autocomplete/accessible-autocomplete.js
+++ src/components/accessible-autocomplete/accessible-autocomplete.js
@@ -9,12 +9,32 @@ AccessibleAutoComplete.prototype.init = function init() {
     const showAllValues = (this.$module.getAttribute('data-show-all-values') === 'true');
     const autoselect = (this.$module.getAttribute('data-auto-select') === 'true');
     const defaultValue = this.$module.getAttribute('data-default-value');
-    window.HMRCAccessibleAutocomplete.enhanceSelectElement({
+
+    const configurationOptions = {
       selectElement: this.$module,
       showAllValues,
       autoselect,
       defaultValue,
-    });
+    };
+
+    const language = this.$module.getAttribute('data-language') || 'en';
+
+    if (language === 'cy') {
+      configurationOptions.tAssistiveHint = () => 'Pan fydd canlyniadau awtolenwi ar gael defnyddiwch saethau i fyny ac i lawr i adolygu a mynd i mewn i ddewis.'
+        + ' Defnyddwyr dyfeisiau cyffwrdd, archwilio trwy gyffwrdd neu gydag ystumiau swipe.';
+      configurationOptions.tStatusQueryTooShort = (minQueryLength) => `Teipiwch ${minQueryLength} neu fwy o nodau ar gyfer canlyniadau`;
+      configurationOptions.tStatusNoResults = () => 'Heb ganfod canlyniadau';
+      configurationOptions.tStatusSelectedOption = (selectedOption, length, index) => `Mae ${selectedOption} ${index + 1} o ${length} wedi'i amlygu`;
+      configurationOptions.tStatusResults = (length, contentSelectedOption) => {
+        const words = {
+          result: (length === 1) ? 'canlyniad' : 'ganlyniad',
+          is: (length === 1) ? 'ar' : 'ar',
+        };
+        return `<span>Mae ${length} ${words.result} ${words.is} gael. ${contentSelectedOption}</span>`;
+      };
+    }
+
+    window.HMRCAccessibleAutocomplete.enhanceSelectElement(configurationOptions);
   }
 };
 
