daddec5330d6872043e1782991f2b2b86fcedfb0
david.ellam <ellamdav@users.noreply.github.com>
2023-11-07 11:36:33 +0000

    PLATUI-2638 updated with Welsh content from WLU

--- src/components/accessible-autocomplete/accessible-autocomplete.js
+++ src/components/accessible-autocomplete/accessible-autocomplete.js
@@ -20,17 +20,17 @@ AccessibleAutoComplete.prototype.init = function init() {
     const language = this.$module.getAttribute('data-language') || 'en';
 
     if (language === 'cy') {
-      configurationOptions.tAssistiveHint = () => 'Pan fydd canlyniadau awtolenwi ar gael defnyddiwch saethau i fyny ac i lawr i adolygu a mynd i mewn i ddewis.'
-        + ' Defnyddwyr dyfeisiau cyffwrdd, archwilio trwy gyffwrdd neu gydag ystumiau swipe.';
-      configurationOptions.tStatusQueryTooShort = (minQueryLength) => `Teipiwch ${minQueryLength} neu fwy o nodau ar gyfer canlyniadau`;
-      configurationOptions.tStatusNoResults = () => 'Heb ganfod canlyniadau';
-      configurationOptions.tStatusSelectedOption = (selectedOption, length, index) => `Mae ${selectedOption} ${index + 1} o ${length} wedi'i amlygu`;
+      configurationOptions.tAssistiveHint = () => 'Pan fydd canlyniadau awtogwblhau ar gael, defnyddiwch y saethau i fyny ac i lawr i’w hadolygu a phwyswch y fysell ’enter’ i’w dewis.'
+        + ' Gall defnyddwyr dyfeisiau cyffwrdd, archwilio drwy gyffwrdd â’r sgrin neu drwy sweipio.';
+      configurationOptions.tStatusQueryTooShort = (minQueryLength) => `Ysgrifennwch ${minQueryLength} neu fwy o gymeriadau am ganlyniadau`;
+      configurationOptions.tStatusNoResults = () => 'Dim canlyniadau chwilio';
+      configurationOptions.tStatusSelectedOption = (selectedOption, length, index) => `Mae ${selectedOption} ${index + 1} o ${length} wedi’i amlygu`;
       configurationOptions.tStatusResults = (length, contentSelectedOption) => {
         const words = {
-          result: (length === 1) ? 'canlyniad' : 'ganlyniad',
-          is: (length === 1) ? 'ar' : 'ar',
+          result: (length === 1) ? 'canlyniad' : 'o ganlyniad',
+          is: (length === 1) ? 'ar' : 'ar', // TODO remove this if it really doesn't vary
         };
-        return `<span>Mae ${length} ${words.result} ${words.is} gael. ${contentSelectedOption}</span>`;
+        return `<span>${length} ${words.result} ${words.is} gael. ${contentSelectedOption}</span>`;
       };
     }
 
