b7b06505d6cbc57efc73821951813acc06fd3daf
David Ellam <ellamdav@users.noreply.github.com>
2023-11-09 15:40:45 +0000

    PLATUI-2638 Enable Welsh language support for accessible-autocomplete (#329)
    PLATUI-2638 supply Welsh translations if language is set to 'cy'; wire up data-min-length; update README to reflect current API

--- src/components/accessible-autocomplete/accessible-autocomplete.js
+++ src/components/accessible-autocomplete/accessible-autocomplete.js
@@ -9,12 +9,32 @@ AccessibleAutoComplete.prototype.init = function init() {
     const showAllValues = (this.$module.getAttribute('data-show-all-values') === 'true');
     const autoselect = (this.$module.getAttribute('data-auto-select') === 'true');
     const defaultValue = this.$module.getAttribute('data-default-value');
-    window.HMRCAccessibleAutocomplete.enhanceSelectElement({
+    const minLength = this.$module.getAttribute('data-min-length');
+
+    const configurationOptions = {
       selectElement: this.$module,
       showAllValues,
       autoselect,
       defaultValue,
-    });
+      minLength,
+    };
+
+    const language = this.$module.getAttribute('data-language') || 'en';
+
+    if (language === 'cy') {
+      configurationOptions.tAssistiveHint = () => 'Pan fydd canlyniadau awtogwblhau ar gael, defnyddiwch y saethau i fyny ac i lawr i’w hadolygu a phwyswch y fysell ’enter’ i’w dewis.'
+        + ' Gall defnyddwyr dyfeisiau cyffwrdd, archwilio drwy gyffwrdd â’r sgrin neu drwy sweipio.';
+      configurationOptions.tStatusQueryTooShort = (minQueryLength) => `Ysgrifennwch ${minQueryLength} neu fwy o gymeriadau am ganlyniadau`;
+      configurationOptions.tNoResults = () => 'Dim canlyniadau wedi’u darganfod';
+      configurationOptions.tStatusNoResults = () => 'Dim canlyniadau chwilio';
+      configurationOptions.tStatusSelectedOption = (selectedOption, length, index) => `Mae ${selectedOption} ${index + 1} o ${length} wedi’i amlygu`;
+      configurationOptions.tStatusResults = (length, contentSelectedOption) => {
+        const resultOrResults = (length === 1) ? 'canlyniad' : 'o ganlyniad';
+        return `${length} ${resultOrResults} ar gael. ${contentSelectedOption}`;
+      };
+    }
+
+    window.HMRCAccessibleAutocomplete.enhanceSelectElement(configurationOptions);
   }
 };
 
--- src/components/accessible-autocomplete/integration.test.js
+++ src/components/accessible-autocomplete/integration.test.js
@@ -98,7 +98,6 @@ describe('enhanceSelectElement on the select element provided', () => {
 
     const input = await page.$('#location-picker');
     await input.click();
-    await page.waitForSelector('#location-picker');
     // eslint-disable-next-line no-param-reassign,no-return-assign
     await page.$eval('#location-picker', (el) => el.value = 'Fr');
     await input.click();
@@ -120,7 +119,6 @@ describe('enhanceSelectElement on the select element provided', () => {
 
     const input = await page.$('#location-picker');
     await input.click();
-    await page.waitForSelector('#location-picker');
     // eslint-disable-next-line no-param-reassign,no-return-assign
     await page.$eval('#location-picker', (el) => el.value = 'Fr');
     await page.waitForSelector('.autocomplete__hint');
@@ -138,7 +136,6 @@ describe('enhanceSelectElement on the select element provided', () => {
 
     const input = await page.$('#location-picker');
     await input.click();
-    await page.waitForSelector('#location-picker');
     // eslint-disable-next-line no-param-reassign,no-return-assign
     await page.$eval('#location-picker', (el) => el.value = 'Fr');
     await input.click();
@@ -148,5 +145,109 @@ describe('enhanceSelectElement on the select element provided', () => {
 
     expect(autocompleteFocused).toBeTruthy();
     expect(Object.keys(visibleElements).length).toEqual(0);
+  });
+
+  it('should render assistive hint in Welsh when data-language is cy', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language/preview`);
+
+    const assistiveHint = await page.evaluate(() => document.querySelector('#location-picker__assistiveHint').textContent);
+
+    expect(assistiveHint.trim()).toEqual(
+      'Pan fydd canlyniadau awtogwblhau ar gael, defnyddiwch y saethau i fyny ac i lawr i’w hadolygu a phwyswch'
+      + ' y fysell ’enter’ i’w dewis. Gall defnyddwyr dyfeisiau cyffwrdd, archwilio drwy gyffwrdd â’r sgrin neu drwy sweipio.',
+    );
+  });
+
+  // for some reason, there are 2 status elements, and content bounces between them as you type...
+  const isAssistiveStatusHintPopulated = () => document.querySelector('#location-picker__status--A').textContent.length > 0
+    || document.querySelector('#location-picker__status--B').textContent.length > 0;
+
+  async function getStatusHint() {
+    return page.evaluate(() => {
+      const assistiveStatusHint = () => document.querySelector('#location-picker__status--A').textContent
+        + document.querySelector('#location-picker__status--B').textContent;
+      return assistiveStatusHint();
+    });
+  }
+
+  it('should render minimum length hint in Welsh when minimum length is specified but not met', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language-and-min-length/preview`);
+
+    const input = await page.$('#location-picker');
+    await input.click();
+
+    // eslint-disable-next-line no-param-reassign,no-return-assign
+    await page.$eval('#location-picker', (el) => el.value = 'Un');
+    await page.waitForFunction(isAssistiveStatusHintPopulated);
+
+    const statusHint = await getStatusHint();
+    expect(statusHint.trim()).toEqual('Ysgrifennwch 3 neu fwy o gymeriadau am ganlyniadau');
+  });
+
+  it('should render status hint in Welsh when data-language is cy and there are multiple matching results', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language/preview`);
+
+    const input = await page.$('#location-picker');
+    await input.click();
+
+    // eslint-disable-next-line no-param-reassign,no-return-assign
+    await page.$eval('#location-picker', (el) => el.value = 'Un');
+    await page.waitForFunction(isAssistiveStatusHintPopulated);
+
+    const statusHint = await getStatusHint();
+    expect(statusHint.trim()).toEqual('2 o ganlyniad ar gael.');
+  });
+
+  it('should render status hint in Welsh when data-language is cy and there is a single matching result', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language/preview`);
+
+    const input = await page.$('#location-picker');
+    await input.click();
+
+    await page.keyboard.press('F');
+    await page.waitForFunction(isAssistiveStatusHintPopulated);
+
+    const statusHint = await getStatusHint();
+    expect(statusHint.trim()).toEqual('1 canlyniad ar gael.');
+  });
+
+  it('should render status hint in Welsh when data-language is cy and there are no matching results', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language/preview`);
+
+    const input = await page.$('#location-picker');
+    await input.click();
+
+    await page.keyboard.press('Z');
+    await page.waitForFunction(isAssistiveStatusHintPopulated);
+
+    const statusHint = await getStatusHint();
+    expect(statusHint.trim()).toEqual('Dim canlyniadau chwilio');
+  });
+
+  it('should render "no results found" dropdown in Welsh when data-language is cy and there are no matching results', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language/preview`);
+
+    const input = await page.$('#location-picker');
+    await input.click();
+
+    await page.keyboard.press('Z');
+    await page.waitForFunction(isAssistiveStatusHintPopulated);
+
+    const statusHint = await page.evaluate(() => document.querySelector('#location-picker__listbox').textContent);
+    expect(statusHint.trim()).toEqual('Dim canlyniadau wedi’u darganfod');
+  });
+
+  it('should render status hint in Welsh when data-language is cy and an item is selected', async () => {
+    await page.goto(`${baseUrl}/components/accessible-autocomplete/with-welsh-language-and-autoselect-on/preview`);
+
+    const input = await page.$('#location-picker');
+    await input.click();
+
+    // eslint-disable-next-line no-param-reassign,no-return-assign
+    await page.$eval('#location-picker', (el) => el.value = 'Un');
+    await page.waitForFunction(isAssistiveStatusHintPopulated);
+
+    const statusHint = await getStatusHint();
+    expect(statusHint.trim()).toEqual('2 o ganlyniad ar gael. Mae United Kingdom 1 o 2 wedi’i amlygu');
   });
 });
