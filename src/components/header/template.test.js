b0ec8050688d805bf4e2133c341fa6215901c3c7
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2024-01-09 14:41:59 +0000

    PLATUI-2627: Fix backstop tests

--- src/components/header/template.test.js
+++ src/components/header/template.test.js
@@ -247,15 +247,6 @@ describe('header', () => {
 
   describe('SVG logo', () => {
     const $ = render('header', {});
-    const $svg = $('.govuk-header__logotype-crown');
-
-    it('sets focusable="false" so that IE does not treat it as an interactive element', () => {
-      expect($svg.attr('focusable')).toEqual('false');
-    });
-
-    it('sets aria-hidden="true" so that it is ignored by assistive technologies', () => {
-      expect($svg.attr('aria-hidden')).toEqual('true');
-    });
 
     describe('fallback PNG', () => {
       const $fallbackImage = $('.govuk-header__logotype-crown-fallback-image');
