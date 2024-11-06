c93153f0ad380c111707cfbd644f72aa478abdb8
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-11-06 12:41:08 +0000

    PLATUI-3341: Updated gulp to v5.0.0, changed glob for copying images, uUpdated streams to set encoding as false for assets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+## [6.38.0] - 2024-11-07
+
+### Changed
+
+- Updated version of gulp to 5.0.0
+
 ## [6.37.0] - 2024-10-23
 
 ### Changed
--- tasks/gulp/copy-to-destination.js
+++ tasks/gulp/copy-to-destination.js
@@ -3,15 +3,15 @@ const destinationPath = require('./destination-path');
 
 gulp.task('copy-govuk-fonts', () => gulp.src([
   'node_modules/govuk-frontend/dist/govuk/assets/fonts/*',
-])
+], { encoding: false })
   .pipe(gulp.dest(`${destinationPath}/govuk/fonts`)));
 
 gulp.task('copy-govuk-images', () => gulp.src([
   'node_modules/govuk-frontend/dist/govuk/assets/images/*',
-])
+], { encoding: false })
   .pipe(gulp.dest(`${destinationPath}/govuk/images`)));
 
 gulp.task('copy-govuk-manifest-json', () => gulp.src([
   'node_modules/govuk-frontend/dist/govuk/assets/manifest.json',
-])
+], { encoding: false })
   .pipe(gulp.dest(`${destinationPath}/govuk`)));
