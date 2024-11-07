2f9fb3af6d5b29f5877008011f9495e0951cc79b
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-11-07 16:53:35 +0000

    PLATUI-3341: Corrected syntax

--- tasks/gulp/dist.js
+++ tasks/gulp/dist.js
@@ -4,4 +4,4 @@ const configPaths = require('../../config/paths.json');
 gulp.task('copy-hmrc-images', () => gulp.src([
   'components/*/images/*',
 ], { cwd: `${configPaths.src}`, encoding: false })
-  .pipe(gulp.dest('dist/components')));
+  .pipe(gulp.dest(`${configPaths.dist}/components`)));
