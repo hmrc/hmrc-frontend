3eee412cc960b79f7ccd73baa91b8599e1121629
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-12-23 12:29:04 +0000

    PLATUI-3352 update playwright

--- tasks/gulp/backstop-config.js
+++ tasks/gulp/backstop-config.js
@@ -78,7 +78,7 @@ module.exports = ({ host, port, components }) => ({
     ci_report: 'backstop_data/ci_report',
   },
   report: ['browser'],
-  engine: 'puppeteer',
+  engine: 'playwright',
   engineOptions: {
     args: ['--no-sandbox'],
   },
