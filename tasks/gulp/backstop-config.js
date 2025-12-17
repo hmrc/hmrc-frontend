583eeb1d031a2ee7a2e71345addabe1b5ba5b951
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2025-12-17 01:43:51 +0000

    only reduce capture limit on jenkins

--- tasks/gulp/backstop-config.js
+++ tasks/gulp/backstop-config.js
@@ -88,7 +88,8 @@ module.exports = ({ host, port, components }) => ({
     args: ['--no-sandbox', '--allow-file-access-from-files', '--disable-web-security'],
   },
   // if running locally, and either stage hangs, try reducing these limits
-  asyncCaptureLimit: 3,
+  // some account-header click/hover/focus examples struggle with higher capture limits in jenkins
+  asyncCaptureLimit: process.env.CI ? 3 : 50,
   asyncCompareLimit: 50,
   debug: false,
   debugWindow: false,
