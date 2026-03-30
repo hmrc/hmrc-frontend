3bcbc98acca8fba2cc5f8e56301087c000624e49
Timothy Bryan <timsb@users.noreply.github.com>
2026-03-30 15:02:21 +0100

    HMRC banner style updates (#530)
    * changed styling of hmrc banner for use with service nav
    
    * bump package version and Changelog

--- src/components/banner/jsdom.test.js
+++ src/components/banner/jsdom.test.js
@@ -6,7 +6,7 @@ import { render, getExamples } from '../../../lib/jest-helpers';
 
 const examples = getExamples('banner');
 
-describe('Internal Header', () => {
+describe('HMRC Header', () => {
   describe('by default', () => {
     it('passes accessibility tests', async () => {
       const $ = render('internal-header', examples.default);
@@ -19,17 +19,20 @@ describe('Internal Header', () => {
 
       expect(results).toHaveNoViolations();
     });
+
     it('should have English text by default', () => {
       const $ = render('banner', examples.default);
-      expect($('.hmrc-banner > .hmrc-organisation-logo > p.govuk-body-s').text().trim()).toEqual('HM Revenue & Customs');
+      expect($('.hmrc-banner > .govuk-width-container > .hmrc-organisation-logo > p.govuk-body-s').text().trim()).toEqual('HM Revenue & Customs');
     });
+
     it('should have Welsh text when specified', () => {
       const $ = render('banner', examples.welsh);
-      expect($('.hmrc-banner > .hmrc-organisation-logo > p.govuk-body-s').text().trim()).toEqual('Cyllid a Thollau EF');
+      expect($('.hmrc-banner > .govuk-width-container > .hmrc-organisation-logo > p.govuk-body-s').text().trim()).toEqual('Cyllid a Thollau EF');
     });
+
     it('should have Tudor Crown logo', () => {
       const $ = render('banner', examples.default);
-      expect($('.hmrc-banner > .hmrc-organisation-logo > svg').html().trim()).toContain('<path class="cls-1" d="m28.5,16.6c');
+      expect($('.hmrc-banner > .govuk-width-container > .hmrc-organisation-logo > svg').html().trim()).toContain('<path class="cls-1" d="m28.5,16.6c');
     });
   });
 });
