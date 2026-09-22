abd5527792850ebffd3c881fed71605d904be85d
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2026-09-22 12:03:55 +0100

    NOJIRA: adjusted threshold

--- tasks/gulp/backstop-config.js
+++ tasks/gulp/backstop-config.js
@@ -89,7 +89,7 @@ module.exports = ({ host, port, components }) => ({
   asyncCompareLimit: 50,
   debug: false,
   debugWindow: false,
-  misMatchThreshold: 0.01,
+  misMatchThreshold: 0.1,
   dockerCommandTemplate: 'docker run --rm -it --network host --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}',
   resembleOutputOptions: {
     ignoreAntialiasing: true,
